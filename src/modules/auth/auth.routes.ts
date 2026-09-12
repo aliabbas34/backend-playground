b75dc4d7f4605362cdd6a52f4f2f1d60676ff19e
aliabbas34 <aliabbas7317@gmail.com>
2026-09-12 16:30:36 +0530

    removed authenticate from the refresh route which was placed there by mistake

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -9,7 +9,7 @@ const authRouter = Router();
 authRouter.post("/signup", validate({ body: signupSchema}), authController.signup);
 authRouter.post("/login", validate({body: loginSchema}), authController.login);
 authRouter.get("/me", authenticate, authController.me )
-authRouter.post("/refresh", authenticate, validate({body: refreshTokenInBodySchema}), authController.refresh);
+authRouter.post("/refresh", validate({body: refreshTokenInBodySchema}), authController.refresh);
 authRouter.post("/logout", authenticate, validate({body: refreshTokenInBodySchema}), authController.logout);
 authRouter.post("/logout-all", authenticate, validate({body: refreshTokenInBodySchema}), authController.logoutAll);
 authRouter.get("/sessions", authenticate, authController.sessions);
